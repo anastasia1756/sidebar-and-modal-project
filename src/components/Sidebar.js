caacd0926945ae614b90201c337532b3d12f9f66
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-12 23:22:05 +0200

    add context

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "../data";
+
+const Sidebar = () => {
+  return (
+    <aside className={`sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => (
+          <li key={id}>
+            <a href={url}>
+              {icon}
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-icons">
+        {social.map(({ id, url, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
